c95b16372bc0b2093a9e059bf77a3f5f240d6a46
hellaskella33 <MAKSYM.KHAMULIAK@lnu.edu.ua>
2024-05-20 20:46:50 +0300

    added some keu events analyze

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ BASE_DIR = Path(os.path.abspath(os.path.dirname(__file__)))
 DATA_DIR = BASE_DIR / 'data'
 ANNOTATED_ARTICLES_FILE = DATA_DIR / 'annotated_articles.json'
 
+KEY_ANNOTATED_ARTICLES_FILE = DATA_DIR / 'key_annotated_articles.json'
